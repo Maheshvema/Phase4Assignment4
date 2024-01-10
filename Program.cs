a7a159d67de47041cb4762b6dc5d787a12155e0a
Maheshvema <babumahesh2088@gmail.com>
2024-01-10 15:52:39 +0530

    safe

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ SortedList<string, Products> products = new SortedList<string, Products>();
 products.Add("Milk", new Products { PName = "Milk", PPrice = 40,PBrand="Amul",ManufacturingDate=new DateTime(2024,1,10),ExpiryDate=new DateTime(2024,1,10) });
 products.Add("GHEE", new Products { PName = "GHEE", PPrice = 100, PBrand = "Amul", ManufacturingDate = new DateTime(2024, 1, 10), ExpiryDate = new DateTime(2024, 1, 10) });
 products.Add("PANEER", new Products { PName ="PANEER", PPrice = 80, PBrand = "Amul", ManufacturingDate = new DateTime(2024, 1, 10), ExpiryDate = new DateTime(2024, 1, 10) });
+products.Add("BUTTER", new Products { PName = "BUTTER", PPrice = 80, PBrand = "Amul", ManufacturingDate = new DateTime(2024, 1, 10), ExpiryDate = new DateTime(2024, 1, 10) });
 Console.WriteLine("Pname \t PPrice \t PBrand \t ManufacturingDate \t ExpiryDate");
 foreach (KeyValuePair<string, Products> productPair in products)
 {
--- obj/Debug/net6.0/ConAppProducts.AssemblyInfo.cs
+++ obj/Debug/net6.0/ConAppProducts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConAppProducts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd0b02761668a8fc6224da8bbc8e792c6a78b3e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConAppProducts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConAppProducts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
